240fc5dcbde4439ac68e3cec30f4df86ac5c6972
Quan-Nguyen <quan16127@gmail.com>
2025-06-20 09:25:12 +0700

    Update Admin

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,13 @@ import { BookingProvider } from "./components/BookingContext";
 import ListBooking from "./components/ListBooking";
 import BookingSuccess from "./components/BookingSuccess";
 import AdminLayout from "./components/admin/AdminLayout";
+import AddSpecialty from "./components/admin/specialties/AddSpecialty";
+import ListSpecialty from "./components/admin/specialties/ListSpecialty";
+import ListDoctor from "./components/admin/doctors/ListDoctor";
+import AddDoctor from "./components/admin/doctors/AddDoctor";
+import AddUser from "./components/admin/user/AddUser";
+import ListUser from "./components/admin/user/ListUser";
+
 
 function AppContent() {
   const location = useLocation();
@@ -36,7 +43,14 @@ function AppContent() {
         <Route path="/list-booking" element={<ListBooking />} />
         <Route path="/booking-success" element={<BookingSuccess />} />
         <Route path="/admin/*" element={<AdminLayout />}>
-          {/* Các route con cho admin ở đây */}
+          <Route path="specialties/add" element={<AddSpecialty />} />
+          <Route path="specialties/list" element={<ListSpecialty />} />
+          <Route path="doctors/add" element={<AddDoctor />} />
+          <Route path="doctors/list" element={<ListDoctor />} />
+          <Route path="users/add" element={<AddUser />} />
+          <Route path="users/list" element={<ListUser />} />
+          
+          {/* Các route con khác */}
         </Route>
       </Routes>
       {!isAdminRoute && <Footer />}
